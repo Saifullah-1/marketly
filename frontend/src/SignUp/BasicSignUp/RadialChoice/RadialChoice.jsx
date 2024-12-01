332024f5dc70bcbc6852bcb809c653147d1e6585
HagerMelook <hagermelook123@gmail.com>
2024-12-01 18:28:16 +0200

    Update the Radial Choice by remove import React

--- frontend/src/SignUp/BasicSignUp/RadialChoice/RadialChoice.jsx
+++ frontend/src/SignUp/BasicSignUp/RadialChoice/RadialChoice.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import backgroundImg from '../../../assets/background.png';
 import './RadialChoice.css';
