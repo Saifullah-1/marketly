d9471e1ecf299ebf530342f2d5eb4e423cabf746
HagerMelook <hagermelook123@gmail.com>
2024-12-01 17:45:17 +0200

    Modify the import of the Image

--- frontend/src/SignUp/BasicSignUp/RadialChoice/RadialChoice.jsx
+++ frontend/src/SignUp/BasicSignUp/RadialChoice/RadialChoice.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import backgroundImg from '../../../assets/backgroung.png';
+import backgroundImg from '../../../assets/background.png';
 import './RadialChoice.css';
 
 function RadialChoice() {
