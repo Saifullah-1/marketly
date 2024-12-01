fc201ada2676b6891be893fe65e902accbb5e5a9
HagerMelook <hagermelook123@gmail.com>
2024-12-01 21:45:34 +0200

    Fix bug

--- backend/src/main/java/com/market/backend/signup/basicSignUp/Controller/VendorController.java
+++ backend/src/main/java/com/market/backend/signup/basicSignUp/Controller/VendorController.java
@@ -1,11 +1,17 @@
 package com.market.backend.signup.basicSignUp.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.market.backend.signup.basicSignUp.Model.BasicVendor;
 import com.market.backend.signup.basicSignUp.Service.VendorService;
 
+@RestController
+@CrossOrigin
+@RequestMapping("/SignUp")
 public class VendorController {
     private final VendorService vendorService;
 
