47e97502ea6b68a0cf7582f6e7773b0ceb5c0e2f
HagerMelook <hagermelook123@gmail.com>
2024-12-01 15:53:56 +0200

    Create the BasicVendor Model

---
+++ backend/src/main/java/com/market/backend/signup/basicSignUp/Model/BasicVendor.java
@@ -0,0 +1,60 @@
+package com.market.backend.signup.basicSignUp.Model;
+
+import java.math.BigInteger;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vendorbasicauth")
+public class BasicVendor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+
+    @Column(name = "businessname", nullable = false, unique = true)
+    private String businessname;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "taxnumber", nullable = false, unique = true)
+    private int taxnumber;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public void setBusinessname(String username) {
+        this.businessname = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBusinessname() {
+        return businessname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getTaxnumber() {
+        return taxnumber;
+    }
+
+    public void setTaxnumber(int taxnumber) {
+        this.taxnumber = taxnumber;
+    }
+
+}
