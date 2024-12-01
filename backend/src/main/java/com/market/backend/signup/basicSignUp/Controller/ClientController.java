3b90d36e526771d5677dfab9739f57f019a1f55e
HagerMelook <hagermelook123@gmail.com>
2024-12-01 16:26:18 +0200

    Create the Client Controller

---
+++ backend/src/main/java/com/market/backend/signup/basicSignUp/Controller/ClientController.java
@@ -0,0 +1,38 @@
+package com.market.backend.signup.basicSignUp.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.market.backend.signup.basicSignUp.Model.BasicClient;
+import com.market.backend.signup.basicSignUp.Service.ClientService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/SignUp")
+public class ClientController {
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @PostMapping("/ClientBasicSignUp")
+    public String clientBasicSignUp(@RequestBody BasicClient client) {
+        String resultMsg;
+        System.out.println("Received User: " + client.getUsername() + ", " + client.getPassword());
+        if (client.getUsername() == null)
+            resultMsg = "The username can't be empty";
+        if (client.getUsername().length() > 80)
+            resultMsg = "The username can't be more than 80 character";
+        if (client.getPassword() == null)
+            resultMsg = "The password can't be empty";
+        if (client.getPassword().length() > 80)
+            resultMsg = "The password can't be more than 80 character";
+        resultMsg = clientService.insertBasicClient(client);
+        System.out.println(resultMsg);
+        return resultMsg;
+    }
+}
