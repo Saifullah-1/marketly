427b1bb7932b9dd357c9f54d20b07070224a636e
HagerMelook <hagermelook123@gmail.com>
2024-12-02 23:30:09 +0200

    Updated with the moved conditions

--- backend/src/main/java/com/market/backend/signup/basicSignUp/Service/ClientService.java
+++ backend/src/main/java/com/market/backend/signup/basicSignUp/Service/ClientService.java
@@ -12,6 +12,14 @@ public class ClientService {
     private BasicClientRepo basicClientRepo;
 
     public String insertBasicClient(BasicClient client) {
+        if (client.getUsername() == null)
+            return "The username can't be empty";
+        if (client.getUsername().length() > 80)
+            return "The username can't be more than 80 character";
+        if (client.getPassword() == null)
+            return "The password can't be empty";
+        if (client.getPassword().length() > 80)
+            return "The password can't be more than 80 character";
         if (basicClientRepo.existsByUsername(client.getUsername())) {
             return "The username is already exist";
         }
--- backend/src/main/java/com/market/backend/signup/basicSignUp/Service/VendorService.java
+++ backend/src/main/java/com/market/backend/signup/basicSignUp/Service/VendorService.java
@@ -12,6 +12,20 @@ public class VendorService {
     private BasicVendorRepo basicVendorRepo;
 
     public String insertBasicVendor(BasicVendor vendor) {
+        if (vendor.getBusinessname() == null)
+            return "The business name can't be empty";
+        if (vendor.getBusinessname().length() > 80)
+            return "The business name can't be more than 80 character";
+        if (vendor.getPassword() == null)
+            return "The password can't be empty";
+        if (vendor.getPassword().length() > 80)
+            return "The password can't be more than 80 character";
+        if (vendor.getTaxnumber() == -1)
+            return "The tax number can't be empty";
+        if (String.valueOf(vendor.getTaxnumber()).length() != 9) {
+            System.out.println(String.valueOf(vendor.getTaxnumber()).length());
+            return "The tax number must be of 9 numbers only";
+        }
         if (basicVendorRepo.existsByBusinessname(vendor.getBusinessname())) {
             return "The business name is already exist";
         }
