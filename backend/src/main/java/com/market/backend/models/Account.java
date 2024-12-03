0062c223fad69147b476c65122c97e668a054b42
Saifullah-1 <siafmosad@gmail.com>
2024-12-03 13:51:30 +0200

    add entities

---
+++ backend/src/main/java/com/market/backend/models/Account.java
@@ -0,0 +1,33 @@
+package com.market.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private boolean isActive;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "username")
+    private String username;
+}
