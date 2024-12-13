d28d4867f037182d5ddc16232dcf90020f1aab44
Mohamed Rezq <mohamedrezq788@gmail.com>
2024-12-14 01:52:51 +0200

    Adding PasswordEncoder

--- backend/src/main/java/com/market/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/com/market/backend/security/SecurityConfiguration.java
@@ -2,14 +2,22 @@ package com.market.backend.security;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,23 +40,35 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+    public UserDetailsService userDetailsManager(DataSource dataSource) {
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
         // define query to retrieve a user by username
         jdbcUserDetailsManager.setUsersByUsernameQuery(
-            "SELECT a.username, p.account_password, a.status AS enabled " +
-            "FROM account a " +
-            "JOIN password p ON a.account_id = p.account_id " +
-            "WHERE a.username = ?");
-
+                "SELECT a.username, p.account_password, a.status AS enabled " +
+                        "FROM account a " +
+                        "JOIN password p ON a.account_id = p.account_id " +
+                        "WHERE a.username = ?");
 
         // define query to retrieve the authorities/roles by username
         jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-            "SELECT a.username, a.type AS role " +
-            "FROM account a " +
-            "WHERE a.username = ?");
+                "SELECT a.username, a.type AS role " +
+                        "FROM account a " +
+                        "WHERE a.username = ?");
 
         return jdbcUserDetailsManager;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder encoder) throws Exception {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(encoder);
+        return new ProviderManager(provider);
     }
 }
