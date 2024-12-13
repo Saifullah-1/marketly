6144bcc3fc87128a6281926f405911df905da8f3
Mohamed Rezq <mohamedrezq788@gmail.com>
2024-12-14 01:06:14 +0200

    updating securty filer chain

--- backend/src/main/java/com/market/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/com/market/backend/security/SecurityConfiguration.java
@@ -1,45 +1,54 @@
 package com.market.backend.security;
 
 import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(customizer -> customizer.disable())
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers(HttpMethod.POST, "/auth/signin").hasRole("USER")
+
+                // .requestMatchers(HttpMethod.GET, "/auth/signin").hasRole("ADMIN")
+                )
+                // .sessionManagement(session ->
+                // session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
         // define query to retrieve a user by username
         jdbcUserDetailsManager.setUsersByUsernameQuery(
-                "SELECT a.username, p.account_password, a.status AS enabled " +
-                        "FROM account a " +
-                        "JOIN password p ON a.account_id = p.account_id " +
-                        "WHERE a.username = ?");
+            "SELECT a.username, p.account_password, a.status AS enabled " +
+            "FROM account a " +
+            "JOIN password p ON a.account_id = p.account_id " +
+            "WHERE a.username = ?");
+
 
         // define query to retrieve the authorities/roles by username
         jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-                "SELECT a.username, a.type AS role " +
-                        "FROM account a " +
-                        "WHERE a.username = ?");
+            "SELECT a.username, a.type AS role " +
+            "FROM account a " +
+            "WHERE a.username = ?");
 
         return jdbcUserDetailsManager;
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(request -> request
-                        .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .build();
     }
 }
