3436dbb76222da0a1855938fd5b96109030dc17a
Mohamed Rezq <mohamedrezq788@gmail.com>
2024-12-13 21:57:15 +0200

    adding UserDetailsManager

--- backend/src/main/java/com/market/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/com/market/backend/security/SecurityConfiguration.java
@@ -1,8 +1,27 @@
 package com.market.backend.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration {
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select account_id, status, type, username, auth_type  from account where username=?");
+
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select  type, username  from account where username=?");
+
+        return jdbcUserDetailsManager;
+    }
 
 }
