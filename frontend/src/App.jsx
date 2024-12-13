c3c319989144efd4c0582bf61bf26a87eecb75d6
HagerMelook <hagermelook123@gmail.com>
2024-12-14 01:29:10 +0200

    modify imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import RadialChoice from "./pages/RadialChoice/RadialChoice";
-import Home from "./pages/Home/Home";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AdminDashboard from "./pages/AdminDashboard";
 import ClientReg from "./pages/ClientReg/ClientReg";
+import Home from "./pages/Home";
+import RadialChoice from "./pages/RadialChoice/RadialChoice";
 import VendorReg from "./pages/VendorReg/VendorReg";
-import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
-import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<RadialChoice />} />
-        <Route path="home" element={<Home isAdmin={false} />} />
+        <Route index element={<RadialChoice />}/>
         <Route path="clientSignUp" element={<ClientReg />} />
         <Route path="vendorSignUp" element={<VendorReg />} />
+        <Route path="home" element={<Home isAdmin={false} />} />
         <Route path="admin-dashboard" element={<AdminDashboard />} />
         <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
