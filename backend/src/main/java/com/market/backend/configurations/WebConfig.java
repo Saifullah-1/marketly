638a157104c99f934a4f38c392159d3a03a1694e
Saifullah-1 <siafmosad@gmail.com>
2024-12-15 14:11:21 +0200

    modify web configurations

--- backend/src/main/java/com/market/backend/configurations/WebConfig.java
+++ backend/src/main/java/com/market/backend/configurations/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")  // Replace with your frontend domain
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:5173")  // Replace with your frontend domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
