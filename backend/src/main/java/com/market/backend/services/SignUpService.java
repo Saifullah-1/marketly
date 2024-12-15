e7496c456247b5d6ce7dce25eeb17fa9fef2b297
Saifullah-1 <siafmosad@gmail.com>
2024-12-15 14:10:30 +0200

    resolve import password

--- backend/src/main/java/com/market/backend/services/SignUpService.java
+++ backend/src/main/java/com/market/backend/services/SignUpService.java
@@ -1,5 +1,6 @@
 package com.market.backend.services;
 
+import com.market.backend.models.Password;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
