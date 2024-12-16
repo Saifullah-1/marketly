4dd3870512340bb2877a497c638dedaed289862c
Saifullah-1 <siafmosad@gmail.com>
2024-12-17 00:21:34 +0200

    Create ProductRepository

---
+++ backend/src/main/java/com/market/backend/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.market.backend.repositories;
+
+import com.market.backend.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    boolean existsByName(String name);
+}
