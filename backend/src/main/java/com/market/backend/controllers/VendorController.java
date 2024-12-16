53cd54edadd3cfd89cb8b635bd17c63d50238100
Saifullah-1 <siafmosad@gmail.com>
2024-12-17 00:28:00 +0200

    Remove unused imports

--- backend/src/main/java/com/market/backend/controllers/VendorController.java
+++ backend/src/main/java/com/market/backend/controllers/VendorController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
