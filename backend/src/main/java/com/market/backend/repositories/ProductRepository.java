cafc365fcd0ae5f7e56c4c5b8835a1d1a7410130
Saifullah-1 <siafmosad@gmail.com>
2024-12-18 00:41:18 +0200

    Remove unnecessary methods

--- backend/src/main/java/com/market/backend/repositories/ProductRepository.java
+++ backend/src/main/java/com/market/backend/repositories/ProductRepository.java
@@ -4,5 +4,4 @@ import com.market.backend.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    boolean existsByName(String name);
 }
