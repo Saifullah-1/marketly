e7818dbc833498586185d45c8d494bc4e6be33de
Ahmed Ali <33ahmedabdelrazek@gmail.com>
2024-12-18 07:13:21 +0200

    add description component

---
+++ frontend/src/components/product page/Description/Description.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import "./Description.css"
+
+function Description ({ product }) {
+  return (
+    <div className="product-info">
+        {product && (<>
+        <h1 className="section-title">Description</h1>
+        <p className="product-description">{product && product.description}</p>
+
+        <h1 className="section-title">Vendor Information</h1>
+        <p className="vendor-info">
+            <strong className="vendor-label">Vendor name: </strong> {product.organizationName}<br/>
+            {product.phoneNumber && (
+            <>
+                <strong className="vendor-label">Phone number: </strong>
+                {product.phoneNumber}
+            </>
+            )}<br/>
+            {product.address && (
+            <>
+                <strong className="vendor-label">Address: </strong>
+                {product.address}
+            </>
+            )}
+        </p>
+        </>)}
+    </div>
+    );
+};
+
+Description.propTypes = {
+    product: PropTypes.object
+};
+
+export default Description;
