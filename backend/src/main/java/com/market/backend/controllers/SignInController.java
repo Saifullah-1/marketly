da2f5f61d13b3e316b2c4fdeea8d88ad97365dc9
Mohamed Rezq <mohamedrezq788@gmail.com>
2024-12-31 23:32:06 +0200

    removing a typo

--- backend/src/main/java/com/market/backend/controllers/SignInController.java
+++ backend/src/main/java/com/market/backend/controllers/SignInController.java
@@ -53,7 +53,6 @@ public class SignInController {
             Map<String, Object> response = new HashMap<>();
             response.put("token", token);
             response.put("id", id);
-            response.put("driverId", id);
             response.put("role", roles);
 
             return ResponseEntity.ok(response);
